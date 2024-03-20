53ee355d492d0875d2816b271a1dc753b1ad2bdd
shawky  sobhy <shawkysobhy98@gmail.com>
2024-03-20 13:44:50 +0200

    fix ui

--- src/ui/ConfrimDuplicate.jsx
+++ src/ui/ConfrimDuplicate.jsx
@@ -1,27 +1,27 @@
-import BrandButton from './BrandButton';
 import StyledButton from './StyledButton';
 function ConfrimDuplicate({ onClose, onDuplicate }) {
 	return (
-		<div className=' w-[480px] px-16 bg-white py-14 rounded-lg'>
+		<div className=' w-[480px] px-16 bg-background py-14 rounded-lg'>
 			<div className='flex flex-col space-y-5'>
 				<p className='text-3xl font-semibold leading-normal text-text'>
 					Duplicate Cabin
 				</p>
-				<p className='mb-5 text-base tracking-wide text-gray-500'>
+				<p className='mb-5 text-base tracking-wide text-text'>
 					by duplicating this cabin new cabin will add to cabin table with all
 					proprites of old cabin but you can custom it after duplicating
 				</p>
 				<div className='flex items-center justify-end space-x-4'>
-					<StyledButton onClick={onClose} sx={'bg-gray-200 '}>
+					<StyledButton onClick={onClose} color='white' sx={'bg-gray-200 '}>
 						Cancel
 					</StyledButton>
-					<BrandButton
+					<StyledButton
+						color='indigo'
 						onClick={() => {
 							onDuplicate();
 							onClose();
 						}}>
 						duplicate
-					</BrandButton>
+					</StyledButton>
 				</div>
 			</div>
 		</div>
