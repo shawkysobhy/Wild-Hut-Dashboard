45e368ae9f8b8c1f76476520069840cab0041880
shawky  sobhy <shawkysobhy98@gmail.com>
2024-03-20 02:11:47 +0200

    fix cabin form

--- src/features/cabins/CabinFormRow.jsx
+++ src/features/cabins/CabinFormRow.jsx
@@ -5,7 +5,7 @@ function CabinFormRow({ children, error }) {
 		<>
 			<div className='grid grid-cols-1 gap-4  sm:grid-cols-[12rem_1fr_1fr] gap-x-10 py-5 items-center border-b border-border'>
 				{children}
-				<SmallErrorMessage message={error.message} />
+				{error && <SmallErrorMessage message={error?.message} />}
 			</div>
 		</>
 	);
