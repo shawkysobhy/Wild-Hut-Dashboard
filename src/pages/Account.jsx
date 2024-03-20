8a3139456da3f5a3d1c362aaf321cd2feff67428
shawky  sobhy <shawkysobhy98@gmail.com>
2024-03-20 14:22:31 +0200

    fix account page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -3,11 +3,11 @@ import UpdateUserDataForm from '../features/authentication/UpdateUserDataForm';
 import UpdatePasswordForm from '../features/authentication/UpdatePasswordForm';
 function Account() {
 	return (
-		<div className=' section-wrapper'>
+		<div className=' section-wrapper text-text'>
 			<div className='flex flex-col space-y-12'>
 				<StyledTitle>Account</StyledTitle>
 				<div className='flex flex-col space-y-8'>
-					<h3 className='ml-4 text-4xl font-medium leading-normal'>
+					<h3 className='ml-4 text-4xl font-medium leading-normal '>
 						Update user data
 					</h3>
 					<UpdateUserDataForm />
